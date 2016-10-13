b95e27ee5182f46c79036ed729a3cd2396c3a81d
Moonmancer <Moonmancer@gmail.com>
2016-10-13 12:55:27 +0200

    Klassen hinzugefügt
    Spiel und Nutzer, sowie Klassendiagramm

---
+++ iScream/Nutzer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iScream
+{
+    class Nutzer
+    {
+
+        private string Vorname
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        private String Nachname
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        private int Nutzer_id
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
--- iScream/Spiel.cs
+++ iScream/Spiel.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace iScream
 {
-    interface IFachkonzept
+    class Spiel
     {
-
     }
 }
