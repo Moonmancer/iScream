61779c3bfdc9892e1b9196bc55c51829d23de747
Moonmancer <Moonmancer@gmail.com>
2016-11-15 13:00:31 +0100

    basic interface implementation

--- iScream/IFachkonzept.cs
+++ iScream/IFachkonzept.cs
@@ -8,6 +8,31 @@ namespace iScream
 {
     interface IFachkonzept
     {
+        #region Details
+           
+        // container detailsUser (Nutzer user);
+        
+        #endregion
+
+        #region Create
+
+        bool createUser(Nutzer user);
+        bool createGame(Spiel game);
+
+        #endregion
+
+        #region Delete
+
+        bool deleteUser(Nutzer user);
+        bool deleteUser(int user_id);
+
+        bool deleteGame(Spiel game);
+        bool deleteGame(int spiel_id)
+        #endregion
+
+        #region Search
+
+        #endregion
 
     }
 }
