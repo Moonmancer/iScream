72e987ceda42a5bf6cbad140138d7ff3a5441f01
Murakai <marcelpennt@gmail.com>
2016-11-24 00:50:44 +0100

    added csv export of user, game and link

--- iScream/Program.cs
+++ iScream/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace iScream
 {
@@ -16,11 +17,13 @@ namespace iScream
             //Main should always start with Settings.Load()
             Settings.Load();
 
-            DHTUI dhtui = new DHTUI(new Datenhaltung2());
+            Datenhaltung2 dh2 = new Datenhaltung2();
+            DHTUI dhtui = new DHTUI(dh2);
 
             dhtui.Run();
 
             //Main should always end with Settings.Save()
+            ToCSV(dh2, ".");
             Settings.Save();
         }
 
@@ -29,6 +32,93 @@ namespace iScream
             to.AddUser(from.GetUser());
             to.AddGame(from.GetGame());
             to.AddLink(from.GetLink());
+        }
+
+        public static void ToCSV(IDatenhaltung from, string toFolder)
+        {
+            DateTime dateTime = DateTime.Now;
+            string dateString = dateTime.ToString("_yyyyMMdd_hhmm");
+
+            PropertyInfo[] userProps = typeof(User).GetProperties();
+            PropertyInfo[] gameProps = typeof(Game).GetProperties();
+            PropertyInfo[] linkProps = typeof(Link).GetProperties();
+
+            //User
+            StringBuilder CSV = new StringBuilder();
+
+            string firstline = "";
+            foreach (PropertyInfo prop in userProps)
+                if (prop.Name != "Name")
+                    if (firstline == "")
+                        firstline = prop.Name;
+                    else
+                        firstline += ";" + prop.Name;
+            CSV.AppendLine(firstline);
+
+            foreach (User cur in from.GetUser())
+            {
+                string newline = "";
+
+                foreach (PropertyInfo prop in userProps)
+                    if (prop.Name != "Name")
+                        if (newline == "")
+                            newline = typeof(User).GetProperty(prop.Name).GetValue(cur).ToString();
+                        else
+                            newline += ";" + typeof(User).GetProperty(prop.Name).GetValue(cur).ToString();
+
+                CSV.AppendLine(newline);
+            }
+            System.IO.File.WriteAllText(System.IO.Path.Combine(toFolder, "User" + dateString + ".csv"), CSV.ToString());
+
+            //Game
+            CSV = new StringBuilder();
+
+            firstline = "";
+            foreach (PropertyInfo cur in gameProps)
+                if (firstline == "")
+                    firstline = cur.Name;
+                else
+                    firstline += ";" + cur.Name;
+            CSV.AppendLine(firstline);
+
+            foreach (Game cur in from.GetGame())
+            {
+                string newline = "";
+
+                foreach (PropertyInfo prop in gameProps)
+                    if (newline == "")
+                        newline = typeof(Game).GetProperty(prop.Name).GetValue(cur).ToString();
+                    else
+                        newline += ";" + typeof(Game).GetProperty(prop.Name).GetValue(cur).ToString();
+
+                CSV.AppendLine(newline);
+            }
+            System.IO.File.WriteAllText(System.IO.Path.Combine(toFolder, "Games" + dateString + ".csv"), CSV.ToString());
+
+            //Link
+            CSV = new StringBuilder();
+
+            firstline = "";
+            foreach (PropertyInfo cur in linkProps)
+                if (firstline == "")
+                    firstline = cur.Name;
+                else
+                    firstline += ";" + cur.Name;
+            CSV.AppendLine(firstline);
+
+            foreach (Link cur in from.GetLink())
+            {
+                string newline = "";
+
+                foreach (PropertyInfo prop in linkProps)
+                    if (newline == "")
+                        newline = typeof(Link).GetProperty(prop.Name).GetValue(cur).ToString();
+                    else
+                        newline += ";" + typeof(Link).GetProperty(prop.Name).GetValue(cur).ToString();
+
+                CSV.AppendLine(newline);
+            }
+            System.IO.File.WriteAllText(System.IO.Path.Combine(toFolder, "Links" + dateString + ".csv"), CSV.ToString());
         }
 
         #region unused code to display data of IDatenhaltung
